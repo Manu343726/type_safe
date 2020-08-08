d35301f6c03b054fbad989f1490cb53c5cc782f2
Manu343726 <Manu343726@gmail.com>
2020-08-08 15:09:30 +0200

    Use FetchContent to manage dependencies

--- external/external.cmake
+++ external/external.cmake
@@ -2,47 +2,31 @@
 # This file is subject to the license terms in the LICENSE file
 # found in the top-level directory of this distribution.
 
-option(TYPE_SAFE_OVERRIDE_SUBMODULE_URLS "Override submodule URLs using cmake config variables" TRUE)
-
-if(NOT TYPE_SAFE_DEBUG_ASSERT_URL)
-    set(TYPE_SAFE_DEBUG_ASSERT_URL https://github.com/foonathan/debug_assert)
+if(NOT TYPE_SAFE_DEBUG_ASSERT_REPO_URL)
+    set(TYPE_SAFE_DEBUG_ASSERT_REPO_URL "https://github.com/foonathan/debug_assert")
+endif()
+if(NOT TYPE_SAFE_DEBUG_ASSERT_VERSION)
+    set(TYPE_SAFE_DEBUG_ASSERT_VERSION "130adcbb393befa29d70036d93905f7bd94aa110")
 endif()
 
-function(configure_submodule module)
-    message(STATUS "Installing ${module} via git submodule")
+include(FetchContent)
 
-    string(TOUPPER "${module}" moduleUpperCase)
-    set(urlVariable "TYPE_SAFE_${moduleUpperCase}_URL")
-
-    find_package(Git REQUIRED)
-
-    if(TYPE_SAFE_OVERRIDE_SUBMODULE_URLS AND ${urlVariable})
-        message(STATUS "Overriding ${module} submodule URL, using ${${urlVariable}}")
-
-        execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory external/${module}
-            WORKING_DIRECTORY ${TYPE_SAFE_SOURCE_DIR})
-
-        execute_process(COMMAND ${GIT_EXECUTABLE} config --file=.gitmodules submodule.${module}.url ${${urlVariable}}
-            WORKING_DIRECTORY ${TYPE_SAFE_SOURCE_DIR})
-
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule sync -- external/${module}
-            WORKING_DIRECTORY ${TYPE_SAFE_SOURCE_DIR})
-    endif()
-
-    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init -- external/${module}
-        WORKING_DIRECTORY ${TYPE_SAFE_SOURCE_DIR})
-endfunction()
-
+FetchContent_Declare(
+    debug_assert
+    GIT_REPOSITORY "${TYPE_SAFE_DEBUG_ASSERT_REPO_URL}"
+    GIT_TAG "${TYPE_SAFE_DEBUG_ASSERT_VERSION}"
+)
 
 find_package(debug_assert QUIET)
 if(debug_assert_FOUND)
     set(TYPE_SAFE_HAS_IMPORTED_TARGETS ON)
 else()
     set(TYPE_SAFE_HAS_IMPORTED_TARGETS OFF)
-    if(TARGET debug_assert)
-        message(STATUS "Using inherited debug_assert target")
-    else()
-        configure_submodule(debug_assert)
-        add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/debug_assert EXCLUDE_FROM_ALL)
+
+    FetchContent_GetProperties(debug_assert)
+
+    if(NOT debug_assert_POPULATED)
+        FetchContent_Populate(debug_assert)
+        add_subdirectory("${debug_assert_SOURCE_DIR}" "${debug_assert_BINARY_DIR}" EXCLUDE_FROM_ALL)
     endif()
 endif()
